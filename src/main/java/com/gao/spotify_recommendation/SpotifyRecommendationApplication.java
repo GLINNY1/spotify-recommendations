3563e8aa7d701da74d9d2823e6e066ec29531ea3
Gao Han Lin <gaohanlin4@gmail.com>
2025-02-01 18:46:42 -0500

    Spring Inital

---
+++ src/main/java/com/gao/spotify_recommendation/SpotifyRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.gao.spotify_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyRecommendationApplication.class, args);
+	}
+
+}
